0f5cc660c638082f255239a11db0379112561038
Paco Luna <pakoo_15@hotmail.com>
2018-10-20 11:31:47 -0500

    Agregamos el menu y los niveles 3 y 4

---
+++ Assets/Scripts/ManejadorJuego.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManejadorJuego : MonoBehaviour {
+    bool JuegoTermino = false;
+    public float restartDelay = 1f;
+    public GameObject completeLeveUI;
+
+    public void CompleteLevel()
+    {
+        completeLeveUI.SetActive(true);
+    }
+
+    public void GameOver()
+    {
+        if(JuegoTermino == false)
+        {
+            JuegoTermino = true;
+            Debug.Log("Game Over");
+            Invoke("Restart", restartDelay);
+        }
+        
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+	
+}
---
+++ Assets/Scripts/StartLevels.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartLevels : MonoBehaviour {
+
+    public void StarGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
