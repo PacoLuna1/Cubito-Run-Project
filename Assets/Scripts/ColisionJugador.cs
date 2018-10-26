0002594a00782bb319f5fb955df9a0d292f2c0b6
Paco Luna <pakoo_15@hotmail.com>
2018-10-26 14:25:00 -0500

    Luna figueroa (#1)
    [CBP] Estructura de proyecto

---
+++ Assets/Scripts/ColisionJugador.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class ColisionJugador : MonoBehaviour {
+    public MovimientoJugador movimiento;
+    public int movimientoSalto;
+    
+    
+       public void OnCollisionEnter(Collision collisionInfo)
+    {
+       if(collisionInfo.collider.tag == "Obstaculo"){
+            movimiento.enabled = false;
+
+            FindObjectOfType<ManejadorJuego>().GameOver();
+            
+        }
+        if (collisionInfo.collider.tag == "Escalones")
+        {
+            movimientoSalto = 0;
+
+        }
+       
+    }
+    
+    
+
+        
+}
---
+++ Assets/Scripts/score.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour {
+    public Transform jugador;
+    public Text scoreText;
+	// Update is called once per frame
+	void Update () {
+        scoreText.text = jugador.position.z.ToString("0");
+	}
+}
