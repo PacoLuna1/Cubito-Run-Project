2f1ac0b4d1a55d2203ce6b6b561da27cf4a7d422
Paco Luna <pakoo_15@hotmail.com>
2018-10-28 13:46:26 -0600

    Se modificaron errores del end

---
+++ Assets/Scripts/Rubio/MenuLevels.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuLevels : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void toLevel1()
+    {
+     //   SceneManager.LoadScene();
+    }
+
+
+    public void toLevel2()
+    {
+     //   SceneManager.LoadScene();
+    }
+
+
+    public void toLevel3()
+    {
+         SceneManager.LoadScene("Nivel 3-Hard");
+    }
+
+
+    public void toLevel4()
+    {
+        SceneManager.LoadScene("Nivel 4-Hard");
+    }
+
+
+    public void toLevel5()
+    {
+        SceneManager.LoadScene("Endless");
+    }
+}
