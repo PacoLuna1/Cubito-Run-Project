c2eda68cccd338a252fcef26d4bb880bebcdebbf
Saul Rubio <saulrubio98@hotmail.com>
2018-11-06 17:33:06 -0600

    [CRP] Menus, Sounds (#10)
    * [CRP] Fix display main menu
    
    * [CRP] Minor changes
    
    - Fix bug Endless
    - Fix menulvls
    - Add prefabs
    
    * [CRP] Add sounds
    
    - It was added the sound of dead in endless
    - It was added the main theme to main menu, and endless

--- Assets/Scripts/Rubio/PlayerMotor.cs
+++ Assets/Scripts/Rubio/PlayerMotor.cs
@@ -6,8 +6,8 @@ public class PlayerMotor : MonoBehaviour {
     public Rigidbody player;
     private CharacterController controller;
     private Vector3 moveVector;
-    private float verticalVelocity = -1.0f;
-    private float speed = 10.0f;
+    private float verticalVelocity = -5.0f;
+    private float speed = 12.0f;
     private float gravity = 12.0f;
     private float animationDuration = 3.0f;
     private bool isDead = false;
@@ -18,7 +18,7 @@ public class PlayerMotor : MonoBehaviour {
         controller = GetComponent<CharacterController>();
         startTime = Time.time;
     }
-	
+
 	// Update is called once per frame
 	void Update () {
 
@@ -28,11 +28,11 @@ public class PlayerMotor : MonoBehaviour {
             return;
         } else
         {
-            Debug.Log("Que onda");
-
-            if (isDead)
+            if (isDead){
                 return;
-            Debug.Log("Que rollo");
+            }
+
+
             moveVector = Vector3.zero;
 
             if (controller.isGrounded)
@@ -45,7 +45,7 @@ public class PlayerMotor : MonoBehaviour {
             }
 
             // X value Left Rigth Desktop
-            moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+            moveVector.x = Input.GetAxisRaw("Horizontal") * (speed * 0.80f);
 
             // X value Left Rigth Phone
             if (Input.GetMouseButton(0))
@@ -75,12 +75,16 @@ public class PlayerMotor : MonoBehaviour {
     // It is begin called every time our capsule hits something
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        if (hit.gameObject.tag == "Enemy" )
-            Death();
+        if (hit.gameObject.tag == "Enemy" ){
+          FindObjectOfType<AudioManager>().Play("dead");
+          Death();
+        }
+
     }
 
     private void Death()
     {
+      FindObjectOfType<AudioManager>().Stop("Theme");
         isDead = true;
         GetComponent<Score>().onDeath();
     }
--- Assets/Scripts/Rubio/Score.cs
+++ Assets/Scripts/Rubio/Score.cs
@@ -13,19 +13,20 @@ public class Score : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        scoreText.text = "???";
+      
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
         if (isDead)
-            return ;
+          return ;
+
 
         if (score >= scoreToNextLevel)
             LevelUp();
         score += Time.deltaTime * difficultyLevel;
         scoreText.text = ((int)score).ToString();
-     
+
 	}
 
     void LevelUp()
