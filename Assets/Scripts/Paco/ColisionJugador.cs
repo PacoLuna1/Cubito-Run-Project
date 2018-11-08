967abb375c81730d36ee067f0a38963f42b3c9c6
Paco Luna <pakoo_15@hotmail.com>
2018-11-08 11:57:48 -0600

    Se completo el diseño de los niveles 3 y 4

--- Assets/Scripts/Paco/ColisionJugador.cs
+++ Assets/Scripts/Paco/ColisionJugador.cs
@@ -4,24 +4,38 @@ public class ColisionJugador : MonoBehaviour {
     public MovimientoJugador movimiento;
     public int movimientoSalto;
     
-    
-       public void OnCollisionEnter(Collision collisionInfo)
+      
+    public void OnCollisionEnter(Collision collisioninfo)
     {
-       if(collisionInfo.collider.tag == "Obstaculo"){
+        if (collisioninfo.collider.tag == "Obstaculo")
+        {
             movimiento.enabled = false;
 
             FindObjectOfType<ManejadorJuego>().GameOver();
-            
+
         }
-        if (collisionInfo.collider.tag == "Escalones")
+        if (collisioninfo.collider.tag == "Escalones")
         {
             movimientoSalto = 0;
 
         }
-       
     }
-    
-    
+    /*
+    public void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.collider.tag == "Obstaculo")
+        {
+            movimiento.enabled = false;
 
-        
+            FindObjectOfType<ManejadorJuego>().GameOver();
+
+        }
+        if (hit.collider.tag == "Escalones")
+        {
+            movimientoSalto = 0;
+
+        }
+    }
+
+    */
 }
--- Assets/Scripts/Paco/MovimientoJugador.cs
+++ Assets/Scripts/Paco/MovimientoJugador.cs
@@ -1,112 +1,123 @@
 ﻿using UnityEngine;
 
 public class MovimientoJugador : MonoBehaviour {
-    //This is a reference to the rigidbody component called "rb"
+
     public Rigidbody rb;
-    //public float fuerzaAdelante = 1f;
-   public float fuerzaLados = 50f;
-    public float MovimientoArriba = 500f;
-    public float MovimientoParar = 800f;
+    private float fuerzaAdelante = 800f;
+    private float fuerzaLados = 50f;
+    private float MovimientoArriba = 500f;
+    private float MovimientoParar = 800f;
 
-    // Phone control
-    public Vector3 moveVector;
-    public float verticalVelocity = -1.0f;
-    public float gravity = 12.0f;
-    public float speed = 10.0f;
-    public float startTime;
-    public CharacterController controller;
-
-
-    // We marked this as "Fixed" Update because
-    // we are using it to mess with physics
-    void Start()
+    
+    // Phone
+    public void Izquierda()
     {
-        controller = GetComponent<CharacterController>();
-        startTime = Time.time;
+        
+        for (int i = 0; i < 4; i++)
+        {
+            rb.AddForce(-fuerzaLados * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+           
+
+        }
     }
-    void FixedUpdate () {
-
-        // rb.AddForce(0, 0, fuerzaAdelante * Time.deltaTime); // Add a force of 2000 on the z-axis
-        // Add Time.deltaTime to express we want to move an object no matter the frames
-
-        //if (Input.GetKey("d"))
-        //{
-         //   rb.AddForce(fuerzaLados * Time.deltaTime, 0, 0,ForceMode.VelocityChange);
-       // }
-        //if (Input.GetKey("a"))
-        //{
-         //   rb.AddForce(-fuerzaLados * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        //}
-
-        if(rb.position.y < -20f)
+     public void Derecha()
+    {
+        
+        for(int i=0; i<4;i++)
         {
-            FindObjectOfType<ManejadorJuego>().GameOver();
+            rb.AddForce(fuerzaLados * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            
+            
         }
+            
+        
+    }
 
-        else{
-
-            moveVector = Vector3.zero;
-
-            if (controller.isGrounded)
-            {
-                verticalVelocity = -0.5f;
-            }
-            else
-            {
-                verticalVelocity -= gravity * Time.deltaTime;
-            }
-
-            // X value Left Rigth Desktop
-            moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
-
-            // X value Left Rigth Phone
-            if (Input.GetMouseButton(0))
-            {
-                // Rigth side of the screen
-                if (Input.mousePosition.x > Screen.width / 2)
-                    moveVector.x = speed;
-                else
-                    moveVector.x = -speed;
-            }
-
-            // Y value Up Down
-            moveVector.y = verticalVelocity;
+    public void Saltar()
+    {
+        FindObjectOfType<ColisionJugador>().movimientoSalto++;
 
 
-            // Z value Foward Backward Desktop
-            moveVector.z = speed;
-            controller.Move(moveVector * Time.deltaTime);
-        }
-
-
-      /*  if (Input.GetKey(KeyCode.W))
+        if (FindObjectOfType<ColisionJugador>().movimientoSalto == 1)
         {
+            rb.AddForce(0, MovimientoArriba * Time.deltaTime, 0, ForceMode.VelocityChange);
+            Debug.Log(FindObjectOfType<ColisionJugador>().movimientoSalto);
 
-            FindObjectOfType<ColisionJugador>().movimientoSalto++;
-
-
-            if (FindObjectOfType<ColisionJugador>().movimientoSalto == 1 )
-            {
-                rb.AddForce(0, MovimientoArriba * Time.deltaTime, 0, ForceMode.VelocityChange);
-                Debug.Log(FindObjectOfType<ColisionJugador>().movimientoSalto);
-
-            }
-            if (FindObjectOfType<ColisionJugador>().movimientoSalto == 4)
-            {
-                rb.AddForce(0, MovimientoArriba * Time.deltaTime, 0, ForceMode.VelocityChange);
-                Debug.Log(FindObjectOfType<ColisionJugador>().movimientoSalto);
-
-            }
-
-
-
-        }*/
-
-
-        if(Input.GetKey("s"))
+        }
+        if (FindObjectOfType<ColisionJugador>().movimientoSalto == 2)
+        {
+            rb.AddForce(0, MovimientoArriba * Time.deltaTime, 0, ForceMode.VelocityChange);
+            Debug.Log(FindObjectOfType<ColisionJugador>().movimientoSalto);
+                
+        }
+    }
+   
+    public void Detener()
+    {
+        
+        for(int i=0; i<40; i++)
         {
             rb.AddForce(0, 0, -MovimientoParar * Time.deltaTime);
+          
         }
-
+        
+        
+       
+        
     }
+
+    // Computer
+    
+     void FixedUpdate()
+     {
+
+
+
+          rb.AddForce(0, 0, fuerzaAdelante * Time.deltaTime); 
+         if (Input.GetKey("d"))
+         {
+             rb.AddForce(fuerzaLados * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+           
+         }
+         if (Input.GetKey("a"))
+         {
+             rb.AddForce(-fuerzaLados * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+         }
+
+        
+        if (Input.GetKey(KeyCode.W) )
+         {
+           
+            FindObjectOfType<ColisionJugador>().movimientoSalto += 1;    
+                
+
+            if (FindObjectOfType<ColisionJugador>().movimientoSalto == 1)
+            {
+                rb.AddForce(0, MovimientoArriba * Time.deltaTime, 0, ForceMode.VelocityChange);
+               
+            }
+            if (FindObjectOfType<ColisionJugador>().movimientoSalto == 2)
+            {
+                rb.AddForce(0, MovimientoArriba * Time.deltaTime, 0, ForceMode.VelocityChange);
+                
+            }
+
+        }
+       
+
+         if (rb.position.y < -20f)
+         {
+             FindObjectOfType<ManejadorJuego>().GameOver();
+         }
+         if (Input.GetKey("s"))
+         {
+             rb.AddForce(0, 0, -MovimientoParar * Time.deltaTime);
+            
+         }
+
+
+ }
+ 
+
+
 }
