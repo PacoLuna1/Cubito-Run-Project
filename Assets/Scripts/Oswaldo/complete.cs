ca15c33bb6910cfd76d88619bb9761414700fcf9
Oswaldo Pineda <carlos-1502010@hotmail.com>
2018-11-08 09:16:30 -0600

    nivel #1 completado (#12)
    excelentes niveles

---
+++ Assets/Scripts/Oswaldo/complete.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class complete : MonoBehaviour {
+
+	public void loadNextLevel ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+
+}
---
+++ Assets/Scripts/Oswaldo/end.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class end : MonoBehaviour {
+
+    public Endscript endscript;
+
+    private void OnTriggerEnter()
+    {
+        endscript.CompleteLevel();
+    }
+}
