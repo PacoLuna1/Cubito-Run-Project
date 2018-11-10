201dc2bf718df1949ffb4bfe1de47ac7ab4fdb90
Saul Rubio <saulrubio98@hotmail.com>
2018-11-10 05:28:32 -0800

    [CRP] Desktop Version

--- Assets/Scripts/Oswaldo/movimientoPlayer.cs
+++ Assets/Scripts/Oswaldo/movimientoPlayer.cs
@@ -4,32 +4,41 @@ public class movimientoPlayer : MonoBehaviour {
 
     public Rigidbody rb;
     private float fuerzaAdelante = 900f;
-    private float fuerzaLados = 400f;
+    private float fuerzaLados = 20f;
 
 
 
     // Phone
     public void Izquierda()
     {
-        
+        if (Input.GetKey("d"))
             rb.AddForce(-fuerzaLados * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
 
 
-        
+
     }
     public void Derecha()
     {
-        
+
             rb.AddForce(fuerzaLados * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
 
-        
+
     }
     // We marked this as "Fixed" Update because
     // we are using it to mess with physics
     void FixedUpdate()
     {
         rb.AddForce(0, 0, fuerzaAdelante * Time.deltaTime ); // Add a force of 2000 on the z-axis
-                                                            // Add Time.deltaTime to express we want to move an object no matter the frames
-      
+       if (Input.GetKey("d"))
+        {
+            rb.AddForce(fuerzaLados * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-fuerzaLados * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+
     }
 }
